049f5df012a75e6724292232f7c5f5515bc58d66
Cassie Yang <wanying1804@gmail.com>
2021-08-04 23:12:09 +1200

    added graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import {
   ApolloClient,
   InMemoryCache,
@@ -10,7 +8,6 @@ import {
 } from "@apollo/client";
 import { Test } from "./test/Test";
 
-import { Route, BrowserRouter, Switch } from "react-router-dom";
 import { TestB } from "./test/TestB";
 
 const client = new ApolloClient({
@@ -21,15 +18,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter>
-        <div className="App">
-          <p>xxxx</p>
-          <Switch>
-            <Route exact path="/SubMenu1" component={Test} />
-            <Route exact path="/SubMenu2" component={TestB} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+     <Test/>
     </ApolloProvider>
   );
 }
