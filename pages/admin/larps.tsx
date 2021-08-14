78eba11d9460a2b0fbc7154b3bdf0c9b5cea5687
Cassie Yang <wanying1804@gmail.com>
2021-08-14 20:51:02 +1200

    added larp table

--- pages/admin/larps.tsx
+++ pages/admin/larps.tsx
@@ -4,6 +4,9 @@ import { useGetAllLarpsQuery } from "src/generated/graphql";
 import ErrorNetwork from "../../components/Error/ErrorNetwork";
 import { Table } from "antd";
 const { Column } = Table;
+import moment from "moment";
+import { faCheckSquare, faUserNinja } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const GET_ALL_LARPS = gql`
   query getAllLarps {
@@ -35,9 +38,33 @@ function Players() {
       <Column title="ID" dataIndex="id" key="id" />
       <Column title="Title" dataIndex="title" key="title" />
       <Column title="Description" dataIndex="description" key="description" />
-      <Column title="Start at" dataIndex="startAt" key="startAt" />
-      <Column title="End at" dataIndex="endAt" key="endAt" />
-      <Column title="Published" dataIndex="isPublished" key="isPublished" />
+      <Column
+        title="Start at"
+        dataIndex="startAt"
+        key="startAt"
+        render={(startAt) => moment(startAt).format("YYYY-MM-DD")}
+      />
+      <Column
+        title="End at"
+        dataIndex="endAt"
+        key="endAt"
+        render={(endAt) => moment(endAt).format("YYYY-MM-DD")}
+      />
+      <Column
+        title="Published"
+        dataIndex="isPublished"
+        key="isPublished"
+        align="center"
+        render={(isPublished) =>
+          isPublished ? (
+            <FontAwesomeIcon
+              size="lg"
+              icon={faCheckSquare}
+              style={{ color: "#56cd25" }}
+            />
+          ) : null
+        }
+      />
     </Table>
   );
 }
